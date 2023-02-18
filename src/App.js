9df012993f31284a40c4154920606f3656ded19e
saqquraza <saqlainaman0@gmail.com>
2023-02-19 02:01:39 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login';
+import { Route,Routes } from 'react-router-dom';
+import { routes } from './routes';
 
 function App() {
   return (
     <div className="App">
-     <Login/>
+      <Routes>
+        <Route path={routes.login} element={<Login/>}/>
+      </Routes>
+    
     </div>
   );
 }
