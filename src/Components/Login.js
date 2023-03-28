e161d814cec1b5ce83c3a03fbffd2a8a562aa68a
saqquraza <saqlainaman0@gmail.com>
2023-03-29 03:55:46 +0530

    changes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,59 +8,65 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [show, setShow] = useState(false);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   const formHandle = (e) => {
     e.preventDefault();
     const user = JSON.parse(localStorage.getItem("user"));
     if (user?.email === email && user?.password === password) {
       console.log("I am logged in");
-      navigate(routes.gameCart)
-      
-    } 
-    else  {
-     alert("Check the input or first create an account ")
-   
+      navigate(routes.gameCart);
+    } else {
+      alert("Check the input or first create an account ");
     }
     setEmail("");
     setPassword("");
   };
 
-  
-
   return (
-    <div>
-      {show ? (
-        <SignUp setShow={setShow}/>
-      ) : (
-        <form onSubmit={formHandle} className="form-container">
+    <div className="parent-cont">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvrMiqGHRoWF-NFHDJQaY_lNPKhdyJEU_HL6aDzugPeQ&usqp=CAU&ec=48600112"
+        alt="game event"
+      />
+      <div>
+        {show ? (
+          <SignUp setShow={setShow} />
+        ) : (
+          <>
+         
           <h2>Login</h2>
-          <input
-            type={"email"}
-            placeholder="Enter Your Email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            className="form-input"
-          ></input>
-          <input
-            type={"password"}
-            placeholder="Enter Your Password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            className="form-input"
-          ></input>
-          <button type="submit" className="form-button">
-            Login
-          </button>
-        </form>
-      )}
-      {show ? (
-        ""
-      ) : (
-        <div onClick={() => setShow(true)} className="create-container">
-         Create an account
-        </div>
-      )}
+          <p style={{margin:"10px",color:"blue"}}>Enter the following details to login</p>
+         
+            <form onSubmit={formHandle} className="form-container">
+              <input
+                type={"email"}
+                placeholder="Enter Your Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                className="form-input"
+              ></input>
+              <input
+                type={"password"}
+                placeholder="Enter Your Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                className="form-input"
+              ></input>
+              <button type="submit" className="form-button">
+                Login
+              </button>
+            </form>
+          </>
+        )}
+        {show ? (
+          ""
+        ) : (
+          <div onClick={() => setShow(true)} className="create-container">
+            Create an account
+          </div>
+        )}
+      </div>
     </div>
   );
 }
